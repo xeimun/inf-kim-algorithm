1ab769866c33d598bc17dbc27baa206b9ef331aa
xeimun <hinote444@naver.com>
2024-12-28 23:58:02 +0900

    인프런 김영한의 코테 입문 Array 8번 문제풀이

---
+++ src/Array/h등수구하기.java
@@ -0,0 +1,29 @@
+package Array;
+
+import java.util.Scanner;
+
+public class h등수구하기 {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) {
+                    cnt++;
+                }
+            }
+            answer[i] = cnt;
+        }
+
+        for (int rank : answer) {
+            System.out.print(rank + " ");
+        }
+    }
+}
